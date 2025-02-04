95dfd29fbe9b12266284b3c6ff1c94c8c90cfa57
Gabriel Braga <bielcatao@gmail.com>
2025-02-04 12:48:37 +0000

    Teste erro

--- tests/checkduplic.sql
+++ tests/checkduplic.sql
@@ -2,6 +2,6 @@ select
     count(*) qtd, 
     company_name, 
     contact_name 
-from {{ref('customers')}}
+from {{source('sources','customers')}}
 group by company_name, contact_name
 having qtd > 1
